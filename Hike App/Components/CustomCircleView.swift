1d62f46556a884ac5a700e470cfad7267cf01bf0
David Cortes <dayareldev@gmail.com>
2025-04-22 17:45:34 +0200

    Added circle animation

---
+++ Hike App/Components/CustomCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCircleView.swift
+//  Hike App
+//
+//  Created by David Cortes Nieto on 22/4/25.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
---
+++ Hike App/Components/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  Hike App
+//
+//  Created by David Cortes Nieto on 22/4/25.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
