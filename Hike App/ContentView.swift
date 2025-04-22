97c50bc5e6c59c7654ef882174b8176d4b8698d1
David Cortes <dayareldev@gmail.com>
2025-04-22 15:25:58 +0200

    Initial Commit

---
+++ Hike App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hike App
+//
+//  Created by David Cortes Nieto on 22/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike App/Hike_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_AppApp.swift
+//  Hike App
+//
+//  Created by David Cortes Nieto on 22/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Hike_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
